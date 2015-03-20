a293c79992f7529865cea8b7e16ce7addd5d9751
Dale Roberts <dale.o.roberts@gmail.com>
2015-03-20 19:33:44 +1100

    script updates

--- doit.sh
+++ doit.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
-#
-#
+#PBS -P v10
+#PBS -q express
+#PBS -l walltime=00:10:00
+#PBS -l ncpus=80
+#PBS -l mem=20GB
+#PBS -j oe
+#PBS -M dale.roberts@anu.edu.au
+#PBS -m abe
+
+if [ "`type -t module`" = 'function' ]; then
+    module load openmpi/1.6.3
+    module load gnuplot
+fi
+
+cd ~/src/kuramoto
+
 INPUT=params.csv
 OLDIFS=$IFS
 IFS=,
@@ -13,7 +27,7 @@ sed 1d $INPUT | while read i graphfile seed ngraphs npaths nsteps alpha lambda s
 do
     M=`wc -l $graphfile | awk '{print $1}'`
     echo "$i $graphfile seed:$seed ngraphs:$M npaths:$npaths nsteps:$nsteps alpha:$alpha lambda:$lambda sigma:$sigma K:$K max_t:$max_t outfile:$outfile" | tee doit.status
-    mpirun -stdin none -c 12 ./kuramoto_mpi $graphfile $M $seed $npaths $nsteps $alpha $lambda $sigma $K $max_t > "$outfile.txt"
+    mpirun -bind-to-none -stdin none -np $PBS_NCPUS ./kuramoto_mpi $graphfile $M $seed $npaths $nsteps $alpha $lambda $sigma $K $max_t > "$outfile.txt"
     export outfile=$outfile
     gnuplot -e "outfile='$outfile.png'" -e "infile='$outfile.txt'" plot.gnuplot
 done
