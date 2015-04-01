9e871f6347c19c118e5721899bf569dcfc2a3934
Dale Roberts <dale.o.roberts@gmail.com>
2015-04-02 09:50:07 +1100

    tweak simulation run

--- doit.sh
+++ doit.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #PBS -P v10
-#PBS -q express
-#PBS -l walltime=00:10:00
-#PBS -l ncpus=80
-#PBS -l mem=20GB
+#PBS -q normal
+#PBS -l walltime=10:00:00
+#PBS -l ncpus=992
+#PBS -l mem=80GB
 #PBS -j oe
+#PBS -r y
 #PBS -M dale.roberts@anu.edu.au
 #PBS -m abe
 
@@ -16,6 +17,8 @@ fi
 cd ~/src/kuramoto
 mkdir -p results
 
+find ~/src/kuramoto -size  0 -print0 |xargs -0 rm
+
 INPUT=params.csv
 OLDIFS=$IFS
 IFS=,
@@ -23,15 +26,25 @@ IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
 cp $INPUT ~/src/kuramoto/results
+cp *.g6 ~/src/kuramoto/results
 cd ~/src/kuramoto/results
 
+# fix line endings
 perl -pi -e 's/\r\n|\n|\r/\n/g' $INPUT
+
+# reverse simulations
+mv $INPUT params.old
+head -1 params.old > $INPUT
+cat params.old | sed 1d | tail -r >> $INPUT
+rm params.old
 
 sed 1d $INPUT | while read i graphfile seed ngraphs npaths nsteps alpha lambda sigma K max_t outfile
 do
     M=`wc -l $graphfile | awk '{print $1}'`
 
     echo "$i $graphfile seed:$seed ngraphs:$ngraphs npaths:$npaths nsteps:$nsteps alpha:$alpha lambda:$lambda sigma:$sigma K:$K max_t:$max_t outfile:$outfile" | tee doit.status
+
+    [ ! -f $graphfile ] && { echo "$graphfile file not found"; exit 99; }
 
     if [ ! -f "$outfile.txt" ]; then
     	mpirun -bind-to-none -stdin none -np $PBS_NCPUS ../kuramoto_mpi $graphfile $M $seed $npaths $nsteps $alpha $lambda $sigma $K $max_t > "$outfile.txt"
